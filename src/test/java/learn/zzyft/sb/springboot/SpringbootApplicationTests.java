940bdf87d8b64dabf20bc2d1eb248e409800a7e2
zzyft <2934361395@qq.com>
2019-12-07 12:58:38 +0800

    init repo

---
+++ src/test/java/learn/zzyft/sb/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.zzyft.sb.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
