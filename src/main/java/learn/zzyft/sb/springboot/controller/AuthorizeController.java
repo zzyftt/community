79e9180f031a34768feba39adcd5ba2a9901a516
zzyft <2934361395@qq.com>
2019-12-08 14:40:43 +0800

    配置application.properties

--- src/main/java/learn/zzyft/sb/springboot/controller/AuthorizeController.java
+++ src/main/java/learn/zzyft/sb/springboot/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import learn.zzyft.sb.springboot.dto.AccesstokenDTO;
 import learn.zzyft.sb.springboot.dto.GithubUser;
 import learn.zzyft.sb.springboot.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,14 +17,20 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientID;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code ,
                            @RequestParam(name = "state")String state){
         AccesstokenDTO accesstokenDTO = new AccesstokenDTO();
-        accesstokenDTO.setClient_id("3590502b3278ec843484");
-        accesstokenDTO.setClient_secret("69ebf503caa026567854c820458c80933deba74a");
+        accesstokenDTO.setClient_id(clientID);
+        accesstokenDTO.setClient_secret(clientSecret);
         accesstokenDTO.setCode(code);
-        accesstokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accesstokenDTO.setRedirect_uri(redirectUri);
         accesstokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accesstokenDTO);
         GithubUser user  = githubProvider.getUser(accessToken);
